38921c5e92b71b2184a338c3138657685c587593
Zrean Tofiq <zrean.tofiq@netlight.com>
2019-11-23 19:29:42 +0100

    Fix formatting

--- DocProvider/PropertyDoc.cs
+++ DocProvider/PropertyDoc.cs
@@ -15,8 +15,8 @@ namespace DocCore.DocProvider
         public override string ToString()
         {
             return
-$@"{Prop.Identifier}
-------
+$@"###{Prop.Identifier}
+
 {Comment.Summary}
 
 ```
